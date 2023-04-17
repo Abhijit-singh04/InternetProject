c97b311e9aaff7b5872a0ed8decfd6213368f84f
Abhijit-singh04 <2001.abhijitsingh@gmail.com>
2023-04-17 10:47:18 +0530

    Initial Commit

---
+++ InternetProject/InternetProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternetProjectApp.swift
+//  InternetProject
+//
+//  Created by Abhijit singh on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct InternetProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
